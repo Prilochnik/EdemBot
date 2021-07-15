0bd70eb3444ee6001ef2bd67bb1b800e7b7cd45d
nikita <prilki.neket@gmail.com>
2021-07-15 18:50:55 +0300

    wefkl

--- src/main/kotlin/com/edem/bot/EdemBot.kt
+++ src/main/kotlin/com/edem/bot/EdemBot.kt
@@ -24,25 +24,88 @@ class EdemBot(
 
     override fun onUpdateReceived(update: Update) {
         if(update.hasMessage()){
-            val apps = appService.getApps()
             val message = update.message
+            val apps = appService.getApps()
             val chatId = message.chatId
             val responseState = if (message.hasText()) {
                 val msgText = message.text
                 when {
                     msgText == "/start" -> MsgStates.StartState
-                    apps.contains(msgText) -> MsgStates.AppChosenState("1")
-                    msgText == Button.addAdsButtonText -> MsgStates.AddAdsIdState("1")
-                    msgText == Button.namingButtonText -> MsgStates.CreateNamingState("1")
-                    msgText == Button.organicButtonText -> MsgStates.AddOrganicState("1")
                     else -> MsgStates.Error
                 }
             } else {
                 MsgStates.Error
             }
-            sendNotification(chatId, responseState)
+            sendInlineMsg(chatId, responseState)
+        }
+        else if(update.hasCallbackQuery()) {
+            val msgText = update.callbackQuery.data
+            val chatId = update.callbackQuery.message.chatId
+            val apps = appService.getApps()
+            val responseState = when {
+                apps.contains(msgText) -> MsgStates.AppChosenState("1")
+                msgText == Button.addAdsButtonText -> MsgStates.AddAdsIdState("1")
+                msgText == Button.namingButtonText -> MsgStates.CreateNamingState("1")
+                msgText == Button.organicButtonText -> MsgStates.AddOrganicState("1")
+                else -> MsgStates.Error
+            }
+            println(responseState)
+            sendInlineMsg(chatId, responseState) //todo needs to update msg
         }
     }
+
+    fun sendInlineMsg(chatId: Long, state : MsgStates) {
+        val responseModel = when(state) {
+            is MsgStates.StartState -> {
+                //responseText = "Ну приветики, на какую прилу желаешь залится?"
+                ResponseModel(
+                        chatId = chatId,
+                        msg = "Ну приветики, на какую прилу желаешь залится?",
+                        buttons = listOf(appService.getApps())
+                )
+            }
+            is MsgStates.AppChosenState -> {
+                ResponseModel(
+                        chatId = chatId,
+                        msg = "Допустим ты выбрал ${state.appId}, дальше то что?",
+                        buttons = listOf(listOf(Button.addAdsButtonText, Button.namingButtonText, Button.organicButtonText))
+                )
+            }
+            else -> {
+                ResponseModel(
+                        chatId = chatId,
+                        msg = "ERorr?",
+                        buttons = listOf(listOf("Error", "Eh ladno"))
+                )
+            }
+        }
+        val responseMessage = SendMessage(chatId, responseModel.msg)
+        responseMessage.setParseMode("Markdown")
+        responseMessage.replyMarkup = Keyboard.getInlineMarkup(responseModel.buttons)
+        execute(responseMessage)
+
+    }
+//    override fun onUpdateReceived(update: Update) {
+//        if(update.hasMessage()){
+//            val apps = appService.getApps()
+//            val message = update.message
+//            val chatId = message.chatId
+//            val responseState = if (message.hasText()) {
+//                val msgText = message.text
+//                when {
+//                    msgText == "/start" -> MsgStates.StartState
+//                    apps.contains(msgText) -> MsgStates.AppChosenState("1")
+//                    msgText == Button.addAdsButtonText -> MsgStates.AddAdsIdState("1")
+//                    msgText == Button.namingButtonText -> MsgStates.CreateNamingState("1")
+//                    msgText == Button.organicButtonText -> MsgStates.AddOrganicState("1")
+//                    else -> MsgStates.Error
+//                }
+//            } else {
+//                MsgStates.Error
+//            }
+//            sendNotification(chatId, responseState)
+//        }
+//    }
 
     private fun sendNotification(chatId: Long, state : MsgStates){
         val responseModel = when(state){
@@ -58,7 +121,7 @@ class EdemBot(
                 ResponseModel(
                         chatId = chatId,
                         msg = "Допустим ты выбрал ${state.appId}, дальше то что?",
-                        buttons = listOf(listOf("Добавить рк", "Сделать нейминг", "Подключить органику(зачем?)"))
+                        buttons = listOf(listOf(Button.addAdsButtonText, Button.namingButtonText, Button.organicButtonText))
                 )
             }
             is MsgStates.AddAdsIdState -> {
--- src/main/kotlin/com/edem/bot/utills/Keyboard.kt
+++ src/main/kotlin/com/edem/bot/utills/Keyboard.kt
@@ -1,6 +1,8 @@
 package com.edem.bot.utills
 
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.InlineKeyboardMarkup
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.ReplyKeyboardMarkup
+import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.InlineKeyboardButton
 import org.telegram.telegrambots.meta.api.objects.replykeyboard.buttons.KeyboardRow
 
 class Keyboard {
@@ -10,6 +12,18 @@ class Keyboard {
             markup.keyboard = allButtons.map { rowButtons ->
                 val row = KeyboardRow()
                 rowButtons.forEach { rowButton -> row.add(rowButton) }
+                row
+            }
+            return markup
+        }
+
+        fun getInlineMarkup(buttons : List<List<String>>) : InlineKeyboardMarkup {
+            val markup = InlineKeyboardMarkup()
+            markup.keyboard = buttons.map { rowButtons ->
+                val row = mutableListOf<InlineKeyboardButton>()
+                rowButtons.forEach { rowButton ->
+                    row.add(InlineKeyboardButton().apply { text = rowButton; callbackData = text })
+                }
                 row
             }
             return markup

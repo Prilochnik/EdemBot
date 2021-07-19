c042d97eb1d8451dbdb248da3a48c26cb7d37ef6
nikita <prilki.neket@gmail.com>
2021-07-19 18:51:07 +0300

    regex

--- src/main/kotlin/com/edem/bot/utills/Extensions.kt
+++ src/main/kotlin/com/edem/bot/utills/Extensions.kt
@@ -1,3 +1,13 @@
 package com.edem.bot.utills
 
 fun String.isInteger() = this.toIntOrNull()?.let { true } ?: false
+
+fun String.isKab() {
+    val regex = """([0-9]*,[0-9]*)*"""
+}
+
+fun main(){
+    val regex = """([0-9])+""".toRegex()
+    println(regex.matches("234345, 234235,"))
+    //println(regex.containsMatchIn("23434g5 , 234235"))
+}
